f7bf70b8a5fef5a8b21cc7a7b1f6787cf421e9e7
Edric Ming <zhekaiming@whu.edu.cn>
2021-07-09 16:56:03 +0800

    T-GCN modeifed

--- examples/recurrent/tgcn_example.py
+++ examples/recurrent/tgcn_example.py
@@ -16,15 +16,15 @@ train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
 class RecurrentGCN(torch.nn.Module):
     def __init__(self, node_features):
         super(RecurrentGCN, self).__init__()
-        self.recurrent = TGCN(node_features, 32)
-        self.linear = torch.nn.Linear(32, 1)
+        self.recurrent = TGCN(node_features, 4, 4)
+        self.linear = torch.nn.Linear(4, 1)
 
     def forward(self, x, edge_index, edge_weight):
         h = self.recurrent(x, edge_index, edge_weight)
         h = F.relu(h)
         h = self.linear(h)
         return h
-        
+
 model = RecurrentGCN(node_features = 4)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -34,18 +34,25 @@ model.train()
 for epoch in tqdm(range(50)):
     cost = 0
     for time, snapshot in enumerate(train_dataset):
+        # time is a int,
+        # snapshot is a data structure representing a week's data (20 nodes, 4 time points on each node)
         y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
-        cost = cost + torch.mean((y_hat-snapshot.y)**2)
-    cost = cost / (time+1)
+        # x is a (20,4) tensor representing the time points,
+        # edge_index is a (2,102) tensor, 102 edges in total
+        # edge_attr is a (102,) tensor, in this dataset edge_attr is 1 for all.
+        # y_hat is a (20,1) tensor
+        cost = cost + torch.mean((y_hat - snapshot.y) ** 2)
+        # customized loss function
+    cost = cost / (time + 1)
     cost.backward()
     optimizer.step()
     optimizer.zero_grad()
-    
+
 model.eval()
 cost = 0
 for time, snapshot in enumerate(test_dataset):
     y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
-    cost = cost + torch.mean((y_hat-snapshot.y)**2)
-cost = cost / (time+1)
+    cost = cost + torch.mean((y_hat - snapshot.y) ** 2)
+cost = cost / (time + 1)
 cost = cost.item()
 print("MSE: {:.4f}".format(cost))
